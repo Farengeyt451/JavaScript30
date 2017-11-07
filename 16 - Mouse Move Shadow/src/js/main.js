e02f40bf2193b508e906dcbbd2d2d46e52ebda57
Alexander <strannik11.92@gmail.com>
2017-11-07 15:39:52 +0300

    JS 16 End

---
+++ 16 - Mouse Move Shadow/src/js/main.js
@@ -0,0 +1,20 @@
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
+const walk = 100;
+
+function shadow(e) {
+	// const width = hero.offsetWidth;
+	// const height = hero.offsetHeight;
+	const {offsetWidth: width, offsetHeight: height} = hero;
+	let {offsetX: x, offsetY: y} = e;
+	if (this !== e.target) {
+		x = x + e.target.offsetLeft;
+		y = y + e.target.offsetRight;
+	}
+	const xWalk = Math.round((x / width * walk) - (walk / 2));
+	const yWalk = Math.round((y / height * walk) - (walk / 2));
+	console.log(xWalk, yWalk);
+	text.style.textShadow = `${xWalk}px ${yWalk}px 0 #BADA55`;
+}
+
+hero.addEventListener("mousemove", shadow);
